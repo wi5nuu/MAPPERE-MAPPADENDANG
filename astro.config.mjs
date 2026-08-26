1e305c40a20fb388e41e4ce9d642e2bc77521887
Wisnu Alfian Nur Ashar <wisnualfian117@gmail.com>
2026-08-26 23:23:36 +0700

    feat: add @astrojs/sitemap, compressHTML, CSS minification to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://labongnge.aksesdesa.id',
+  integrations: [
+    sitemap({
+      changefreq: 'monthly',
+      priority: 1.0,
+      lastmod: new Date(),
+    }),
+  ],
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  compressHTML: true,
+  vite: {
+    build: {
+      cssMinify: true,
+    },
+  },
 });
